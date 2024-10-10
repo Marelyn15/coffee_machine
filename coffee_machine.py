8fcac6428a52fc866499547c189afb52139b9b44
Marelyn Deschamps <78377414+Marelyn15@users.noreply.github.com>
2024-10-09 19:36:16 -0700

    Update coffee_machine.py

--- coffee_machine.py
+++ coffee_machine.py
@@ -12,55 +12,83 @@ def defining_money():
     quarter = float(quarter) * 0.25
     change = penny + dime + nickel + quarter
     return round(change,2)
+
 change = 0
-your_deserve = input("What would you like (espresso/latte/cappuccino): ").lower()
 
-if your_deserve == "report":
-    print(f"Water: {resources["water"]}")
-    print(f"Milk: {resources["milk"]}")
-    print(f"Coffe: {resources["coffee"]}")
-    print(f"Money: {change}")
+stop = 0
+while stop == 0:
+    your_deserve = input("What would you like (espresso/latte/cappuccino): ").lower()
+    
+    if your_deserve == "report":
+        
+        print(f"Water: {resources["water"]}")
+        print(f"Milk: {resources["milk"]}")
+        print(f"Coffe: {resources["coffee"]}")
+        print(f"Money: {change}")
+    # First option
+    elif your_deserve == "espresso":
+        change = defining_money()
 
-elif your_deserve == "espresso":
-    change = defining_money()
-    if change > MENU[your_deserve]["cost"] :
-        change = change - MENU[your_deserve]["cost"]
-        print(f"Here is your {your_deserve}, enjoy!")
-        print(f"Your change is {change}")
+        if change > MENU[your_deserve]["cost"] :
+    
+            if resources["water"] >= MENU[your_deserve]["ingredients"]["water"] and resources["coffee"] >= MENU[your_deserve]["ingredients"]["coffee"]:
 
-        resources["water"] = resources["water"] - MENU[your_deserve]["ingredients"]["water"]
-        resources["coffee"] = resources["coffee"] - MENU[your_deserve]["ingredients"]["coffee"]
+                change = change - MENU[your_deserve]["cost"]
+                print(f"Here is your {your_deserve}, enjoy!")
+                print(f"Your change is {change}")
 
+                resources["water"] = resources["water"] - MENU[your_deserve]["ingredients"]["water"]
+                resources["coffee"] = resources["coffee"] - MENU[your_deserve]["ingredients"]["coffee"]
+
+            else:
+                print("Sorry this machine doesn't have enough ingredients. :(")
+
+        else:
+            print("You don't have enough money, sorry")
+
+    elif your_deserve == "latte":
+        change = defining_money()
+        if change > MENU[your_deserve]["cost"]:
+            if resources["water"] >= MENU[your_deserve]["ingredients"]["water"] and resources["coffee"] >= MENU[your_deserve]["ingredients"]["coffee"] and resources["milk"] >= MENU[your_deserve]["ingredients"]["milk"]:
+                change = change - MENU[your_deserve]["cost"]
+                print(f"Here is your {your_deserve}, enjoy!")
+                print(f"Your change is {change}")
+
+                resources["water"] = resources["water"] - MENU[your_deserve]["ingredients"]["water"]
+                resources["milk"] = resources["milk"] - MENU[your_deserve]["ingredients"]["milk"]
+                resources["coffee"] = resources["coffee"] - MENU[your_deserve]["ingredients"]["coffee"]
+
+            else:
+                print("Sorry this machine doesn't have enough ingredients. :(")
+                stop = 1
+
+        else:
+            print("You don't have enough money, sorry")
+            stop = 1
+
+    elif your_deserve == "cappuccino":
+        change = defining_money()
+        if change > MENU[your_deserve]["cost"]:
+            if resources["water"] >= MENU[your_deserve]["ingredients"]["water"] and resources["coffee"] >= MENU[your_deserve]["ingredients"]["coffee"] and resources["milk"] >= MENU[your_deserve]["ingredients"]["milk"]:
+                change = change - MENU[your_deserve]["cost"]
+                print(f"Here is your {your_deserve}, enjoy!")
+                print(f"Your change is {change}")
+
+                resources["water"] = resources["water"] - MENU[your_deserve]["ingredients"]["water"]
+                resources["milk"] = resources["milk"] - MENU[your_deserve]["ingredients"]["milk"]
+                resources["coffee"] = resources["coffee"] - MENU[your_deserve]["ingredients"]["coffee"]
+
+            else:
+                print("Sorry this machine doesn't have enough ingredients. :(")
+                stop = 1
+
+        else:
+            print("You don't have enough money, sorry")
+            stop = 1
+
+    elif your_deserve == "off":
+        print("The coffee machine is off")
+        stop = 1
     else:
-        print("You don't have enough money, sorry")
-
-elif your_deserve == "latte":
-    change = defining_money()
-    if change > MENU[your_deserve]["cost"]:
-        change = change - MENU[your_deserve]["cost"]
-        print(f"Here is your {your_deserve}, enjoy!")
-        print(f"Your change is {change}")
-
-        resources["water"] = resources["water"] - MENU[your_deserve]["ingredients"]["water"]
-        resources["milk"] = resources["milk"] - MENU[your_deserve]["ingredients"]["milk"]
-        resources["coffee"] = resources["coffee"] - MENU[your_deserve]["ingredients"]["coffee"]
-
-    else:
-        print("You don't have enough money, sorry")
-elif your_deserve == "cappuccino":
-    change = defining_money()
-    if change > MENU[your_deserve]["cost"]:
-        change = change - MENU[your_deserve]["cost"]
-        print(f"Here is your {your_deserve}, enjoy!")
-        print(f"Your change is {change}")
-
-        resources["water"] = resources["water"] - MENU[your_deserve]["ingredients"]["water"]
-        resources["milk"] = resources["milk"] - MENU[your_deserve]["ingredients"]["milk"]
-        resources["coffee"] = resources["coffee"] - MENU[your_deserve]["ingredients"]["coffee"]
-
-    else:
-        print("You don't have enough money, sorry")
-elif your_deserve == "off":
-    print("The coffee machine is off")
-else:
-    print("You need to select a least one option of these above type of coffe... ")
+        print("You need to select a least one option of these above type of coffe... ")
+        stop = 1
